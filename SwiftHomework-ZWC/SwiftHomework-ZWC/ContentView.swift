2954967cbd2cf407f1b3521c750d71732179cc0f
油菜花 <651263878@qq.com>
2022-02-16 17:15:19 +0800

    [Add] - Creat project.

---
+++ SwiftHomework-ZWC/SwiftHomework-ZWC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftHomework-ZWC
+//
+//  Created by 油菜花 on 2022/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
